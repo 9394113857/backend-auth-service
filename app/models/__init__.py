29dfe0bc6593e7afae80f025a45c5ca569ab57ac
Raghunadh Barlapudi <81347796+9394113857@users.noreply.github.com>
2026-05-10 23:16:50 +0530

    Expose additional model classes in __init__.py
    Added new model classes for password history, reset token, email verification token, refresh token, OTP verification, user address, and user session.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,23 @@
-"""
-This file simply exposes the model classes
-so that Flask-Migrate (Alembic) can discover them.
-"""
-
 from .user import User
 from .token_blacklist import TokenBlocklist
+from .password_history import PasswordHistory
+from .password_reset_token import PasswordResetToken
+from .email_verification_token import EmailVerificationToken
 
-__all__ = ["User", "TokenBlocklist"]
+from .refresh_token import RefreshToken
+from .otp_verification import OTPVerification
+from .user_address import UserAddress
+from .user_session import UserSession
+
+
+__all__ = [
+    "User",
+    "TokenBlocklist",
+    "PasswordHistory",
+    "PasswordResetToken",
+    "EmailVerificationToken",
+    "RefreshToken",
+    "OTPVerification",
+    "UserAddress",
+    "UserSession"
+]
